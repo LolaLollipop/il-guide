d3ade172e6d62e81115eb5ef0d1ea8be59e891cc
Rue <135553058+Ruemena@users.noreply.github.com>
2023-09-23 20:56:35 -0500

    Create MyFirstTranspiler.cs

---
+++ Code/MyFirstTranspiler.cs
@@ -0,0 +1,27 @@
+[HarmonyPatch(typeof(VoiceTransceiver), nameof(VoiceTransceiver.ServerReceiveMessage))]
+public static class MyFirstTranspiler {
+    private static VoiceChatChannel TutorialHearSCPs(VoiceChatChannel channel, ReferenceHub speaker, ReferenceHub listener) {
+        if (speaker.GetRoleId() == RoleTypeId.Tutorial && listener.IsSCP()) return VoiceChatChannel.RoundSummary; else return channel;
+    }
+
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
+        List<CodeInstruction> newInstructions = ListPool<CodeInstruction>.Shared.Rent(instructions);
+        int index = newInstructions.FindIndex(instruction =>
+            instruction.opcode == OpCodes.Callvirt
+            && (MethodInfo)instruction.operand == Method(typeof(VoiceModuleBase), nameof(VoiceModuleBase.ValidateReceive)));
+        index += 1;
+
+        newInstructions.InsertRange(index, new[]
+        {
+            new CodeInstruction(OpCodes.Ldarg_1),
+            new CodeInstruction(OpCodes.Ldfld, Field(typeof(VoiceMessage), nameof(VoiceMessage.Speaker))),
+            new CodeInstruction(OpCodes.Ldloc_3),
+            new CodeInstruction(OpCodes.Call, Method(typeof(MyFirstTranspiler), nameof(TutorialHearSCPs)))
+        });
+
+        foreach (CodeInstruction instruction in newInstructions)
+            yield return instruction;
+
+        ListPool<CodeInstruction>.Shared.Return(newInstructions);
+    }
+}
